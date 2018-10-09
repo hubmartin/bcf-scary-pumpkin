7cc8c5e1d7918e83f45e96f57b745377bf36b29e
hubmartin <hub.martin@gmail.com>
2018-10-09 16:43:26 +0200

    first commit

--- app/application.h
+++ app/application.h
@@ -5,6 +5,22 @@
 #define VERSION "vdev"
 #endif
 
+#ifndef LED_STRIP_COUNT
+#define LED_STRIP_COUNT 144
+#endif
+
+#ifndef LED_STRIP_TYPE
+#define LED_STRIP_TYPE 4
+#endif
+
 #include <bcl.h>
+
+typedef struct
+{
+    uint8_t channel;
+    float value;
+    bc_tick_t next_pub;
+
+} event_param_t;
 
 #endif // _APPLICATION_H
